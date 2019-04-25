6d37e16b4f7f11830c1ef491e5eeadb8efe20231
cnhexin <bupthexin@hotmail.com>
2019-04-24 23:52:31 -0400

    changeurl

--- service/main.go
+++ service/main.go
@@ -27,7 +27,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.231.169.70:9200"
+	ES_URL = "http://10.142.0.2:9200"
 )
 
 type Location struct {
